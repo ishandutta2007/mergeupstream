e7627c9c55902d7ee69f5d11a32d5daa1a1b5930
Ishan Dutta <ishandutta2007@gmail.com>
2020-06-26 09:04:08 +0530

    first

---
+++ mergeupstream/mergeupstream.sh
@@ -0,0 +1,14 @@
+function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(pwd)"
+IFS='/' read -ra PARTS <<< "$DIR"
+echo $DIR
+echo $IFS
+echo $PARTS[@]
+
+#git remote add upstream https://github.com/whoever/whatever.git
+#git checkout master
+#git pull upstream master
+#git push
+
